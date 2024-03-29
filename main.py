8f39ce61eb2e6a04b4a11d24bf2900a287987943
SamuelFabian <s.f.z@hotmail.com>
2024-03-29 00:02:42 -0600

    Se agregó la opción 4 para el usuario

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from funciones_calculadora import sumar_n_numeros
+from funciones_calculadora import multiplicacion_n_numeros
+from funciones_calculadora import division_2_numeros
+from funciones_calculadora import resolver_para_y
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 while True:
@@ -9,6 +15,7 @@ while True:
     1.-Hacer una suma de N números.
     2.-Hacer una multiplicación de N números.
     3.-Hacer una división de dos números.
+    4.-Resolver la posición en Y (Para la ecuación y = mx + b).
           
     0.-Salir del programa.
     ''')
@@ -16,9 +23,25 @@ while True:
     opcion = int(input('> '))
     if opcion == 0:
         break
+    
     elif opcion == 1:
         resultado = sumar_n_numeros()
         print(f'El resultado de tu suma es {resultado}')
+
+    elif opcion == 2:
+        resultado = multiplicacion_n_numeros()
+        print(f'El resultado es {resultado}')
+
+    elif opcion == 3:
+        resultado = division_2_numeros()
+        print(f'El resultado es {resultado}')
+
+    elif opcion == 4:
+        resultado = resolver_para_y()
+        print(f'El resultado es {resultado}')
+
+    else:
+        print('Ejecuta una opción válida :( ')
 
 
 print('-------------------------------------')
