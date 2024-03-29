d4a32f87606607a15e9dd5181b7a3df844e4f2bf
SamuelFabian <s.f.z@hotmail.com>
2024-03-29 00:01:43 -0600

    Se agregó la función que permite calcular Y para una recta

--- funciones_calculadora.py
+++ funciones_calculadora.py
@@ -8,3 +8,33 @@ def sumar_n_numeros():
 
     return sum(lista_numeros)
 
+
+def multiplicacion_n_numeros():
+    resultado = 1
+    numeros_a_multiplicar = int(input('Cuántos números deseas multiplicar: '))
+    for numero in range (0, numeros_a_multiplicar):
+        numeros_a_multiplicar = float(input(f'Ingresa el {numero + 1} a multiplicar: '))
+        resultado *= numeros_a_multiplicar
+
+    return resultado
+
+
+def division_2_numeros():
+    numerador = float(input('Ingresa el numerador: '))
+    denominador = float(input('Ingresa el denominador: '))
+    if denominador == 0:
+        print('No puedes dividir entre 0')
+        return None
+    
+    return numerador / denominador
+
+# y = mx + b
+def resolver_para_y():
+    pendiente = float(input('Por favor ingrese la pendiente: '))
+    ordenada_al_origen = float(input('Por favor ingrese la ordenada al origen: '))
+    punto_en_x = int(input('Ingrese un punto en x: '))
+
+    return (pendiente * punto_en_x) + ordenada_al_origen
+
+
+
